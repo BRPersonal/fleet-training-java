79820600c8ba3f73b4770b717586a7a610f1f883
BRPersonal <balajirengan@gmail.com>
2025-05-02 14:02:22 +0530

    Added Three.java

---
+++ src/test/java/com/fleet/training/test/Three.java
@@ -0,0 +1,10 @@
+package com.fleet.training.test;
+
+public class Three implements Runnable
+{
+    @Override
+    public void run()
+    {
+        System.out.println("line 1 code in feature 3");
+    }
+}
