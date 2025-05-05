9751b2ecd442471976d647570e49045a4b4092d3
BRPersonal <balajirengan@gmail.com>
2025-05-05 13:15:56 +0530

    Five.java added

---
+++ src/test/java/com/fleet/training/test/Five.java
@@ -0,0 +1,10 @@
+package com.fleet.training.test;
+
+public class Five implements Runnable
+{
+    @Override
+    public void run()
+    {
+        System.out.println("Feature 5 code added");
+    }
+}
